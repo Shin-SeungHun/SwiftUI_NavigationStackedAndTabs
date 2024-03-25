c4ad80c28174d9fe3d2a853cdd21db246d7f0b70
shin-SeungHun <sng7387@gmail.com>
2024-03-25 17:13:24 +0900

    feat: custom tabView에 페이지 연결

--- NavigationStackedAndTabs/ContentView.swift
+++ NavigationStackedAndTabs/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab: Tab = .home
+    
+    init() {
+        // 탭바를 감췄을때는 터치가 안되도록
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack (alignment: .bottom) {
+            TabView(selection: $currentTab){
+                Text("홈")
+                    .tag(Tab.home)
+                Text("게시판")
+                    .tag(Tab.forum)
+                Text("스터디")
+                    .tag(Tab.study)
+                Text("프로필")
+                    .tag(Tab.profile)
+            }
+//            기본 탭뷰 사용할때는 이렇게 사용
+//            .toolbar(.hidden, for: .tabBar)
+            
+            CustomTabView(currentTab: $currentTab)
         }
-        .padding()
+        
     }
 }
 
